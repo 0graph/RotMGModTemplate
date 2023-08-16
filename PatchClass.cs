6286e2b165b5118c7f9dad3651eef5df9aed817a
him <im@him.is>
2023-08-17 00:09:08 +0100

    Fix assembly references, ShowFloatingText, add comments, fix readability/typos
    - Fix assembly references in .csproj to work on any system with MelonLoader installed
    - Fix ShowFloatingText function and add overload for any color
    - Add comments/fix typos

--- PatchClass.cs
+++ PatchClass.cs
@@ -1,18 +1,21 @@
-﻿using Harmony;
-using Il2Cpp;
+﻿using Il2Cpp;
 
 namespace BasicMelonMod.Patches
 {
-    //
-    [HarmonyPatch(typeof(ALCOHEIDGKL))] // This is the Class that 0Harmony is Patching - in this case ALCOHEIDGKL is the Tile Class for RotMG
-    [HarmonyPatch("HILBBBOHAAH")] // This is the Method that will be patched - Every time it is called the patch will run, Prefix runs the patch before the method call and postfix runs after the method call
+    // This is the class that HarmonyX is patching - in this case ALCOHEIDGKL is the Tile class for RotMG.
+    [HarmonyLib.HarmonyPatch(typeof(ALCOHEIDGKL))]
+    // This is the method that will be patched. Every time it is called in-game it will run through the patch below.
+    // Prefix() runs the patch before the method call and Postfix() runs it after. Check MelonLoader documentation for more info.
+    [HarmonyLib.HarmonyPatch("HILBBBOHAAH")]
     public static class FogPatch
     {
 
-        [HarmonyPrefix]
-        public static bool Prefix(Il2Cpp.ALCOHEIDGKL __instance, int __0)
+        [HarmonyLib.HarmonyPrefix]
+        public static bool Prefix(ALCOHEIDGKL __instance, int __0)
         {
             return false;
         }
     }
+
+    // Add more Harmony patches here or expand the mod!
 }
